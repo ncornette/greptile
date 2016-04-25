7b1cddbfef68a7f5203e4064e3adcdb304ead164
Nicolas Cornette <nicolas.cornette@gmail.com>
2016-04-25 14:32:11 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Greptile',
+      version='0.8',
+      description='Grep implementation with replace',
+      author='Nicolas Cornette',
+      author_email='',
+      url='https://github.com/ncornette/greptile',
+      packages=[],
+      scripts=['greptile.py']
+      )
